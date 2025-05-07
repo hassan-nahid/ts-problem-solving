75a6dd3e70c93e6ee881da4686c660cdcaaafc3e
Nahid-subi <scientisthassannahid@gmail.com>
2025-05-07 23:00:01 +0600

    some change

--- solution.ts
+++ solution.ts
@@ -1,9 +1,5 @@
 function formatString(input: string, toUpper?: boolean): string {
-    if (toUpper === false) {
-        return input.toLowerCase();
-    } else {
-        return input.toUpperCase();
-    }
+    return toUpper === false ? input.toLowerCase() : input.toUpperCase();
 }
 
 
